6956cf53de02e149b7365752797668ed6b10ab61
Juhyeon Kim <shadow98a@gmail.com>
2023-08-09 23:59:00 +0900

    Created statistics API, API specification and API JSON samples for them.

--- TJSManager/src/main/java/com/tjs/tjsmanager/domain/scm/SalesConsumer.java
+++ TJSManager/src/main/java/com/tjs/tjsmanager/domain/scm/SalesConsumer.java
@@ -1,8 +1,5 @@
 package com.tjs.tjsmanager.domain.scm;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,27 +10,16 @@ import lombok.Data;
 //구매자 기록
 @Data
 @Entity
-@Table(name = "SALES_CONSUMER")
+@Table(name="SALES_CONSUMER")
 public class SalesConsumer {
 //	판매 번호
-	@Column(name = "SALES_NUM")
 	@Id
-	@GeneratedValue
-	private Long salesNum;
-
-//	구매자 성별
-	@Column(name = "CONSUMER_GENDER")
-	private String consumerGender;
-
-//	구매자 나이대
-	@Column(name = "CONSUMER_AGE")
-	private Integer consumerAge;
 	
-//	판매 일시
-	@Column(name = "SALES_DATE")
-	private LocalDateTime salesDate = LocalDateTime.now();
-
-//	판매 메모
-	@Column(nullable = true)
-	private String memo;
+	private SalesRecord salesNum;
+	
+//	구매자 성별
+	private String consumerGender;
+	
+//	구매자 나이대
+	private Long consumerAge;
 }
