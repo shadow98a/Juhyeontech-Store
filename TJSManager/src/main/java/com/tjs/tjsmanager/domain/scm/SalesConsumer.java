cc27655728cb2f45e58bc98bb340a9c3e9f4aa23
Juhyeon Kim <shadow98a@gmail.com>
2023-08-12 08:39:49 +0900

    Created statistics.

--- TJSManager/src/main/java/com/tjs/tjsmanager/domain/scm/SalesConsumer.java
+++ TJSManager/src/main/java/com/tjs/tjsmanager/domain/scm/SalesConsumer.java
@@ -1,5 +1,8 @@
 package com.tjs.tjsmanager.domain.scm;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,16 +13,27 @@ import lombok.Data;
 //구매자 기록
 @Data
 @Entity
-@Table(name="SALES_CONSUMER")
+@Table(name = "SALES_CONSUMER")
 public class SalesConsumer {
 //	판매 번호
+	@Column(name = "SALES_NUM")
 	@Id
-	
-	private SalesRecord salesNum;
-	
+	@GeneratedValue
+	private Long salesNum;
+
 //	구매자 성별
+	@Column(name = "CONSUMER_GENDER")
 	private String consumerGender;
-	
+
 //	구매자 나이대
-	private Long consumerAge;
+	@Column(name = "CONSUMER_AGE")
+	private Integer consumerAge;
+	
+//	판매 일시
+	@Column(name = "SALES_DATE")
+	private LocalDateTime salesDate = LocalDateTime.now();
+
+//	판매 메모
+	@Column(nullable = true)
+	private String memo;
 }
